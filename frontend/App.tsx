d30c903f7b2e0f5cc470ba441977eb07376ee626
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-06-30 09:24:40 +0300

    Comment Logic For Initialize Firebase App

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,22 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-import firebase from "firebase";
+// import firebase from "firebase";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
 
-if (firebase.apps.length === 0) {
-  firebase.initializeApp({
-    apiKey: "AIzaSyCkxB5hQCHehkCEV1Nund2-8rlcBpRdnuY",
-    authDomain: "vibevision-app-mobile.firebaseapp.com",
-    projectId: "vibevision-app-mobile",
-    storageBucket: "vibevision-app-mobile.appspot.com",
-    messagingSenderId: "13103397525",
-    appId: "1:13103397525:web:07b94dc4e54e48cd47f99c",
-    measurementId: "G-VVP4F2QHX2",
-  });
-}
+// if (firebase.apps.length === 0) {
+//   firebase.initializeApp({
+//     apiKey: "AIzaSyCkxB5hQCHehkCEV1Nund2-8rlcBpRdnuY",
+//     authDomain: "vibevision-app-mobile.firebaseapp.com",
+//     projectId: "vibevision-app-mobile",
+//     storageBucket: "vibevision-app-mobile.appspot.com",
+//     messagingSenderId: "13103397525",
+//     appId: "1:13103397525:web:07b94dc4e54e48cd47f99c",
+//     measurementId: "G-VVP4F2QHX2",
+//   });
+// }
 
 export default function App() {
   return (
