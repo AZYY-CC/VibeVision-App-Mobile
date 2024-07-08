813ba0e842ec3598d58333db01ae3a3e74788d59
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-07-08 10:27:10 +0300

    Setting Up React Query In Project

--- client/App.tsx
+++ client/App.tsx
@@ -1,12 +1,21 @@
 import { Provider } from "react-redux";
 import { store } from "./store";
 import RootNavigation from "./navigations/RootNavigation";
-import 'react-native-get-random-values'
+import "react-native-get-random-values";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const App = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: { refetchInterval: false, staleTime: Infinity },
+    },
+  });
+
   return (
     <Provider store={store}>
-      <RootNavigation />
+      <QueryClientProvider client={queryClient}>
+        <RootNavigation />
+      </QueryClientProvider>
     </Provider>
   );
 };
