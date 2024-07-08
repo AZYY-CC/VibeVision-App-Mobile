bf54091f76238ed8c2016b4e61e53fa9e61991e9
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-07-08 13:34:38 +0300

    Implement a Like System For Videos

--- client/screens/HomeFeedScreen/PostSingle/PostSingleOverlay/index.tsx
+++ client/screens/HomeFeedScreen/PostSingle/PostSingleOverlay/index.tsx
@@ -1,9 +1,36 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
-import React from "react";
-import styles from "./styles";
+import React, { useEffect, useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
+import { useSelector } from "react-redux";
+import { getLikeById, updateLike } from "../../../../services/posts";
+
+import styles from "./styles";
 
 const PostSingleOverlay = ({ user, post }) => {
+  const currentUser = useSelector((state) => state.auth.currentUser);
+  const [currentLikeState, setCurrentLikeState] = useState({
+    state: false,
+    counter: post.likesCount,
+  });
+
+  useEffect(() => {
+    getLikeById(post.id, currentUser.uid).then((res) => {
+      console.log(res)
+      setCurrentLikeState({
+        counter: post.likesCount,
+        state: res,
+      });
+    });
+  }, []);
+
+  const handleUpdateLike = () => {
+    setCurrentLikeState({
+      state: !currentLikeState.state,
+      counter: currentLikeState.counter + (currentLikeState.state ? -1 : 1),
+    });
+    updateLike(post.id, currentUser.uid, currentLikeState.state);
+  };
+
   return (
     <View style={styles.container}>
       <View>
@@ -16,9 +43,18 @@ const PostSingleOverlay = ({ user, post }) => {
           <Image style={styles.avatar} source={{ uri: user?.photoURL }} />
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.actionButton}>
-          <Ionicons color="white" size={40} name={"heart-outline"} />
-          <Text style={styles.actionButtonText}>0</Text>
+        <TouchableOpacity
+          style={styles.actionButton}
+          onPress={() => handleUpdateLike(currentLikeState)}
+        >
+          <Ionicons
+            color={currentLikeState.state ? "red" : "white"}
+            size={40}
+            name={currentLikeState.state ? "heart" : "heart-outline"}
+          />
+          <Text style={styles.actionButtonText}>
+            {currentLikeState.counter}
+          </Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.actionButton}>
--- client/services/posts.ts
+++ client/services/posts.ts
@@ -1,4 +1,10 @@
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  runTransaction,
+} from "firebase/firestore";
 import { db } from "../configs/firebase";
 
 export const getFeed = async () => {
@@ -14,5 +20,44 @@ export const getFeed = async () => {
     return posts;
   } catch (error) {
     return Promise.reject(error);
+  }
+};
+
+export const getLikeById = async (postId, uid) => {
+  try {
+    const likeDoc = doc(db, "posts", postId, "likes", uid);
+    const res = await getDoc(likeDoc);
+
+    return res.exists();
+  } catch (error) {
+    throw new Error("Failed to retrieve like data");
+  }
+};
+
+export const updateLike = async (postId, uid, currentLikeState) => {
+  try {
+    const postRef = doc(db, "posts", postId);
+    const likeDoc = doc(postRef, "likes", uid);
+
+    await runTransaction(db, async (transaction) => {
+      const postDoc = await transaction.get(postRef);
+      if (!postDoc.exists()) {
+        throw new Error("Post does not exist");
+      }
+
+      const newLikesCount = currentLikeState
+        ? postDoc.data().likesCount - 1
+        : postDoc.data().likesCount + 1;
+
+      if (currentLikeState) {
+        transaction.delete(likeDoc);
+      } else {
+        transaction.set(likeDoc, {});
+      }
+
+      transaction.update(postRef, { likesCount: newLikesCount });
+    });
+  } catch (error: any) {
+    throw new Error("Failed to update like state: " + error.message);
   }
 };
