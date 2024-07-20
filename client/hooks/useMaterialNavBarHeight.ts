bc34939bbb552ec1c244c54462d466297991ccea
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-07-20 12:04:53 +0300

    Fix Bug In useMaterialNavBarHeight Hook

--- client/hooks/useMaterialNavBarHeight.ts
+++ client/hooks/useMaterialNavBarHeight.ts
@@ -3,7 +3,7 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 const useMaterialNavBarHeight = (withoutBottomTabs: any) => {
   const { bottom, top } = useSafeAreaInsets();
   console.log({ bottom, top });
-  return bottom - Math.floor(top) + (withoutBottomTabs ? 0 : 140);
+  return bottom - Math.floor(top) + (withoutBottomTabs ? 20 : 140);
 };
 
 export default useMaterialNavBarHeight;
